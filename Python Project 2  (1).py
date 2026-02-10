f11725981bab1d3e9c4fdeaa901d8aa378e7ec61
devasree011 <devasree011@gmail.com>
2026-02-10 11:29:12 +0530

    Add files via upload

---
+++ Python Project 2  (1).py
@@ -0,0 +1,208 @@
+score = 0
+
+print("WELCOME TO PYTHON MCQ QUIZ")
+print("---------------------------")
+
+# Question 1
+print("\n1. Python is a _____ language")
+print("a) Low level")
+print("b) High level")
+print("c) Machine")
+print("d) Assembly")
+ans = input("Enter answer: ")
+if ans == 'b':
+    score += 1
+
+# Question 2
+print("\n2. Which symbol is used for comments in Python?")
+print("a) //")
+print("b) /* */")
+print("c) #")
+print("d) %")
+ans = input("Enter answer: ")
+if ans == 'c':
+    score += 1
+
+# Question 3
+print("\n3. What is the correct extension of Python file?")
+print("a) .pt")
+print("b) .pyt")
+print("c) .py")
+print("d) .python")
+ans = input("Enter answer: ")
+if ans == 'c':
+    score += 1
+
+# Question 4
+print("\n4. Which keyword is used to define a function?")
+print("a) fun")
+print("b) def")
+print("c) function")
+print("d) define")
+ans = input("Enter answer: ")
+if ans == 'b':
+    score += 1
+
+# Question 5
+print("\n5. Which data type is used to store text?")
+print("a) int")
+print("b) float")
+print("c) str")
+print("d) bool")
+ans = input("Enter answer: ")
+if ans == 'c':
+    score += 1
+
+# Question 6
+print("\n6. What is the output of 5 + 2?")
+print("a) 7")
+print("b) 52")
+print("c) 10")
+print("d) Error")
+ans = input("Enter answer: ")
+if ans == 'a':
+    score += 1
+
+# Question 7
+print("\n7. Which loop is used when number of iterations is known?")
+print("a) while")
+print("b) for")
+print("c) do-while")
+print("d) loop")
+ans = input("Enter answer: ")
+if ans == 'b':
+    score += 1
+
+# Question 8
+print("\n8. Which operator is used for multiplication?")
+print("a) x")
+print("b) *")
+print("c) %")
+print("d) /")
+ans = input("Enter answer: ")
+if ans == 'b':
+    score += 1
+
+# Question 9
+print("\n9. Which function is used to take input?")
+print("a) input()")
+print("b) scan()")
+print("c) read()")
+print("d) get()")
+ans = input("Enter answer: ")
+if ans == 'a':
+    score += 1
+
+# Question 10
+print("\n10. What does len() do?")
+print("a) Finds length")
+print("b) Adds values")
+print("c) Deletes data")
+print("d) Sorts data")
+ans = input("Enter answer: ")
+if ans == 'a':
+    score += 1
+
+# Question 11
+print("\n11. Which keyword is used to exit a loop?")
+print("a) stop")
+print("b) end")
+print("c) break")
+print("d) exit")
+ans = input("Enter answer: ")
+if ans == 'c':
+    score += 1
+
+# Question 12
+print("\n12. Which data type stores True or False?")
+print("a) int")
+print("b) float")
+print("c) bool")
+print("d) str")
+ans = input("Enter answer: ")
+if ans == 'c':
+    score += 1
+
+# Question 13
+print("\n13. Which symbol is used for assignment?")
+print("a) ==")
+print("b) =")
+print("c) :=")
+print("d) <=")
+ans = input("Enter answer: ")
+if ans == 'b':
+    score += 1
+
+# Question 14
+print("\n14. Which loop runs until condition is false?")
+print("a) for")
+print("b) while")
+print("c) if")
+print("d) switch")
+ans = input("Enter answer: ")
+if ans == 'b':
+    score += 1
+
+# Question 15
+print("\n15. Which keyword is used for condition?")
+print("a) if")
+print("b) when")
+print("c) case")
+print("d) check")
+ans = input("Enter answer: ")
+if ans == 'a':
+    score += 1
+
+# Question 16
+print("\n16. Which function prints output?")
+print("a) output()")
+print("b) display()")
+print("c) print()")
+print("d) show()")
+ans = input("Enter answer: ")
+if ans == 'c':
+    score += 1
+
+# Question 17
+print("\n17. What is the output of 10//3?")
+print("a) 3.3")
+print("b) 3")
+print("c) 4")
+print("d) Error")
+ans = input("Enter answer: ")
+if ans == 'b':
+    score += 1
+
+# Question 18
+print("\n18. Which symbol is used for modulus?")
+print("a) &")
+print("b) %")
+print("c) *")
+print("d) //")
+ans = input("Enter answer: ")
+if ans == 'b':
+    score += 1
+
+# Question 19
+print("\n19. Which keyword is used to define class?")
+print("a) class")
+print("b) define")
+print("c) struct")
+print("d) object")
+ans = input("Enter answer: ")
+if ans == 'a':
+    score += 1
+
+# Question 20
+print("\n20. Python was developed by?")
+print("a) Dennis Ritchie")
+print("b) James Gosling")
+print("c) Guido van Rossum")
+print("d) Elon Musk")
+ans = input("Enter answer: ")
+if ans == 'c':
+    score += 1
+
+print("\n---------------------------")
+print("Quiz Completed!")
+print("Your Score:", score, "/ 20")
